3dd86d6e8de6a544f5c34ab1a40f8b23a511a15f
Carlos Godoy <carlosgodoy576@gmail.com>
2020-08-25 22:09:29 -0600

    Modificacion de CandidatoFacade funciones necesarias dentro de Candidato Facade

--- BITLAB-ejb/src/java/sv/edu/bitlab/beans/CandidatoFacade.java
+++ BITLAB-ejb/src/java/sv/edu/bitlab/beans/CandidatoFacade.java
@@ -5,9 +5,12 @@
  */
 package sv.edu.bitlab.beans;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import sv.edu.bitlab.entidades.Candidato;
 
 /**
@@ -16,7 +19,9 @@ import sv.edu.bitlab.entidades.Candidato;
  */
 @Stateless
 public class CandidatoFacade extends AbstractFacade<Candidato> {
-
+    // varibales y lista de tipo candidatos.
+    List<Candidato> listaCandidado = new ArrayList<>();
+    private Candidato candidato = new Candidato();
     @PersistenceContext(unitName = "BITLAB-ejbPU")
     private EntityManager em;
 
@@ -28,5 +33,77 @@ public class CandidatoFacade extends AbstractFacade<Candidato> {
     public CandidatoFacade() {
         super(Candidato.class);
     }
+     public List<Candidato> candidatoPorEstadoAplicacion (String estadoAplicacion) throws Exception {
+      try {
+            Query q = em.createQuery("select c from Candidato c where c.eapId.eapNombre= :estadoAplicacion");
+            q.setParameter("estadoAplicacion", estadoAplicacion);
+           return q.getResultList();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } 
+    }
     
+    
+    public List<Candidato> candidatosPotencialList() {
+        List<Candidato> listaCandidado = new ArrayList<>();
+
+        return listaCandidado = CandidatoFacade.super.findAll();
+
+    }
+    
+    public List<Candidato> CandidatoTipoPRe(){
+        if (candidato.getEapId().equals(1)){
+           
+           return listaCandidado = CandidatoFacade.super.findAll();
+        }
+        
+        return null;
+    }
+    
+      public List<Candidato> candidatoPre () throws Exception {
+      try {
+            Query q = em.createQuery("select c from Candidato c where c.eapId.eapId = 1");
+           return q.getResultList();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } 
+    }
+      
+      //lista de candidatos potenciales
+     public List<Candidato> candidatoPotencial () throws Exception {
+      try {
+            Query q = em.createQuery("select c from Candidato c where c.genId.genInternet='si' and c.genId.genComputadora='si' and c.genId.genTiempo='si' and c.eapId.eapId=9");
+           return q.getResultList();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } 
+    }
+     //lista de candidatos Regular alto
+       public List<Candidato> candidatoRegularAlto () throws Exception {
+      try {
+            Query q = em.createQuery("select c from Candidato c where c.genId.genInternet='si' and c.genId.genComputadora='si' and c.genId.genTiempo='no' and c.eapId.eapId=9");
+           return q.getResultList();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } 
+    }
+       
+        public List<Candidato> candidatoRegularBajo () throws Exception {
+      try {
+            Query q = em.createQuery("select c from Candidato c where c.genId.genInternet='si' and c.genId.genComputadora='no' and c.genId.genTiempo='no' and c.eapId.eapId=9");
+           return q.getResultList();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } 
+    }
+        
+           public List<Candidato> candidatoSinOportunidad () throws Exception {
+      try {
+            Query q = em.createQuery("select c from Candidato c where c.genId.genInternet='no' and c.genId.genComputadora='no' and c.genId.genTiempo='no' and c.eapId.eapId=9");
+           return q.getResultList();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } 
+    }
+       
 }
