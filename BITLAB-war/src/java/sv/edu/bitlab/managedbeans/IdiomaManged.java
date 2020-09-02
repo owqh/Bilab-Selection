dd469caf7686add82a0aa201395acc5055b1d442
Manuel Ramos <67672732+Manuel-R31@users.noreply.github.com>
2020-09-02 08:23:05 -0600

    Idioma Managed.

---
+++ BITLAB-war/src/java/sv/edu/bitlab/managedbeans/IdiomaManged.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.bitlab.managedbeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import sv.edu.bitlab.beans.IdiomaFacade;
+import sv.edu.bitlab.entidades.Idioma;
+import sv.edu.bitlab.utilidades.Utilidades;
+
+/**
+ *
+ * @author Manuel
+ */
+@Named(value = "idiomaManged")
+@SessionScoped
+public class IdiomaManged implements Serializable {
+    
+    private static final Logger log = Logger.getLogger(DocenteManaged.class.getName());
+    
+    @EJB
+    private IdiomaFacade idiomaFacade;
+    
+    private Idioma idioma;
+    private List<Idioma> idiomaLista = null;
+    
+    /**
+     * Creates a new instance of IdiomaManged
+     */
+    public IdiomaManged() {
+    }
+    
+    @PostConstruct
+    private void encontrarEntidades(){
+        idioma = new Idioma();
+        idiomaLista = idiomaFacade.findAll();
+        log.info("Lista de entidades cargada");
+    }
+    
+    
+    public void nuevaEntidad() throws NoSuchMethodException{
+        try {
+            idioma = Idioma.class.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            Logger.getLogger(IdiomaManged.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        log.info("Entidad creada exitosamente!!");
+    }
+     
+    public void eliminarEntidad(){
+        try {
+            idiomaFacade.remove(idioma);
+            encontrarEntidades();
+            Utilidades.lanzarInfo("Exitoso ", Idioma.class.getSimpleName() + " ha sido eliminado");
+        } catch (Exception ex) {
+            Logger.getLogger(IdiomaManged.class.getName()).log(Level.SEVERE, null, ex);
+            Utilidades.lanzarError("Error al eliminar ", ex.getMessage());
+        }
+        log.info("Entidad eliminada");
+    }
+    
+    
+    public void guardarEntidad(){
+        try {
+            idiomaFacade.edit(idioma);
+            encontrarEntidades();
+            Utilidades.lanzarInfo("Exitoso ", Idioma.class.getSimpleName() + " ha sido guardado");
+        } catch (Exception ex) {
+            Logger.getLogger(IdiomaManged.class.getName()).log(Level.SEVERE, null, ex);
+            Utilidades.lanzarError("Error al guardar ", ex.getMessage());
+        }
+        log.info("Entidad editada y guardada");
+    }
+    
+
+    public IdiomaFacade getIdiomaFacade() {
+        return idiomaFacade;
+    }
+
+    public void setIdiomaFacade(IdiomaFacade idiomaFacade) {
+        this.idiomaFacade = idiomaFacade;
+    }
+
+    public Idioma getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(Idioma idioma) {
+        this.idioma = idioma;
+    }
+
+    public List<Idioma> getIdiomaLista() {
+        return idiomaLista;
+    }
+
+    public void setIdiomaLista(List<Idioma> idiomaLista) {
+        this.idiomaLista = idiomaLista;
+    }
+    
+}
